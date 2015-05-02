2235dd0626a7a202a23774298cb479d18b5e2f10
sarathrami <sarathrami0@gmail.com>
2015-05-02 18:44:11 -0400

    Added random verification from truth data

--- cf.py
+++ cf.py
@@ -1,9 +1,13 @@
 from __future__ import division
 from math import sqrt
+import pickle
+import powerlaw
+import random
 
-MAX_FRIENDS_ALLOWED = 15
+MAX_FRIENDS_ALLOWED = 30
 usr_venue_rating = {}
 friends = {}
+user_frnd_dist = {}
 
 def loadData(path="."):
     """
@@ -13,6 +17,17 @@ def loadData(path="."):
         (id, longitude, latitude) = line.split("|")
         usrs[0] = (int(id), float(longitude), float(latitude))
         print usrs[0]
+
+    user_frnd_dist = pickle.load(open(path + 'friends_gyr_equals.txt', 'rb'))
+    data = []
+    for usr in user_frnd_dist:
+        frnd_dst = user_frnd_dist[usr]
+        for fd in frnd_dst:
+            data.append(fd[1])
+
+    results = powerlaw.Fit(data)
+    print results.power_law.alpha
+    print results.power_law.xmin
     """
     for line in open(path + "ratings.dat"):
         try:
@@ -87,6 +102,14 @@ def pearson_simple_similarity(uid1, uid2, cache={}):
 
     return cache[(uid1, uid2)]
 
+def gyr_simple_similarity(uid1, uid2, cache={}):
+    if (uid1, uid2) in cache:
+        return cache[(uid1, uid2)]
+
+
+
+    return cache[(uid1, uid2)]
+
 def getMostSimilarNScores_Usrs(usr, n=5, fn_similarity = pearson_simple_similarity):
     similarity_vals = [(fn_similarity(usr, curr_usr), curr_usr)
               for curr_usr in usr_venue_rating if curr_usr != usr]
@@ -99,7 +122,7 @@ def getRecommendations(usr, n=-1, fn_similarity = pearson_simple_similarity):
     total_similarity_sums_for_vid = {}
 
     for curr_usr in usr_venue_rating:
-        if curr_usr == usr or curr_usr not in friends[usr]:
+        if curr_usr == usr or usr not in friends or curr_usr not in friends[usr]:
             continue
 
         similarity_scr = fn_similarity(usr, curr_usr)
@@ -118,11 +141,61 @@ def getRecommendations(usr, n=-1, fn_similarity = pearson_simple_similarity):
     recommendations.reverse()
     return recommendations[0:n]
 
+
+def randomVenueRemoval(usr, per_rem=20):
+    removed_venue_rating_list = []
+    if usr in usr_venue_rating:
+        venues_ratings = usr_venue_rating[usr]
+        while len(venues_ratings) > 0 and per_rem > 100 * len(removed_venue_rating_list)/ len(venues_ratings):
+            venue = venues_ratings.keys()[random.randrange(0, len(venues_ratings))]
+            removed_venue_rating_list.append((venue, venues_ratings[venue]))
+            del venues_ratings[venue]
+
+    return removed_venue_rating_list
+
+def randomVerification(iterations = 1, per_rem=20, fn_similarity = pearson_simple_similarity):
+
+    hit = 0
+    runs = 0
+    i = 0
+    usrs = list(usr_venue_rating.keys())
+    while usrs and len(usrs) and i < iterations:
+        usr = usrs.pop(random.randrange(0, len(usrs)))
+        solution = randomVenueRemoval(usr, per_rem=20)
+
+        if not solution or not len(solution):
+            print "Nothing removed."
+            continue
+        else:
+            i += 1
+
+        venues_removed = [x[0] for x in solution]
+
+        print 'User [%d] randomly selected for test...' % usr
+        print 'Venues removed for User [%d] are' % usr, venues_removed
+        found_solution = getRecommendations(usr, n=-1, fn_similarity=fn_similarity)
+        venues_recovered = [x[1] for x in found_solution]
+        print 'Venues recovered for User [%d] are' % usr, venues_recovered
+        success_ratio = len(set(venues_recovered).intersection(set(venues_removed))) / (len(venues_removed))
+        hit += success_ratio
+        runs += 1
+
+        # put back the ratings
+        for vr in solution:
+            usr_venue_rating[usr][vr[0]] = vr[1]
+
+        print 'Success ratio is {0}'.format(100*int(success_ratio))
+        print '-'*50
+
+    print 'Avg success ratio is {0}'.format(100*int(hit)/runs)
+
 if __name__ == '__main__':
     loadData("./data/")
+    randomVerification(iterations=25, per_rem=20, fn_similarity=cosine_similarity)
+
     for uid in friends:
         if uid == 5:
             print 'Friends of [%d] are =' % uid, friends[uid]
             print getRecommendations(uid, fn_similarity = pearson_simple_similarity)
             #print getRecommendations(uid, fn_similarity = cosine_similarity)
-            print '-'*50
+            print '-'*50                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
